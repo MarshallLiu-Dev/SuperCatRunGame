2476e1ce10a893f71f922154af92f611fee147b0
MarshallLiu-Dev <paulomartins12358@gmail.com>
2023-01-17 17:57:06 -0300

    secound commit

--- Script/script.js
+++ Script/script.js
@@ -1,36 +1,41 @@
+const cat = document.querySelector(".cat");
+const tijolo = document.querySelector(".tijolo");
+const score = document.querySelector(".score");
+let alreadyJump = false;
+let count = 0;
 
-const cat = document.querySelector('.cat');
-const tijolo = document.querySelector('.tijolo');
+document.addEventListener("keydown", (e) => {
+  if ((e.code === "ArrowUp") | (e.code === "Space")) {
+    jump();
+  }
+});
 
+function jump() {
+  if (!cat.classList.contains("jump")) {
+    cat.classList.add("jump");
+    alreadyJump = true;
 
-const jump = () => {
-    cat.classList.add('jump');
-
-    setTimeout(() =>{
-         cat.classList.remove('jump');
-    }, 500);
+    setTimeout(() => {
+      cat.classList.remove("jump");
+      alreadyJump = false;
+    }, 1100);
+  }
 }
 
-const loop = setInterval(() => {
-    
-    const tijoloPosition = tijolo.offsetLeft;
-    const catPosition = +window.getComputedStyle(cat).bottom.replace('px', '');
-    console.log(catPosition)
+setInterval(() => {
+  let catBottom = parseInt(
+    window.getComputedStyle(cat).getPropertyValue("bottom")
+  );
+  let tijoloLeft = parseInt(
+    window.getComputedStyle(tijolo).getPropertyValue("left")
+  );
 
-    if(tijoloPosition <= 120 && tijoloPosition > 0 && catPosition < 90 ){
-      tijolo.style.animation = 'none';
-      tijolo.style.left = `${tijoloPosition}px`;
+  if (tijoloLeft > 40 && tijoloLeft < 270 && catBottom <= 50 && !alreadyJump) {
+    alert(`Game Over! Seu score foi: ${count}`);
+    count = 0;
 
-      cat.style.animation = 'none';
-      cat.style.bottom = `${catPosition}px`;
+  }
 
-     cat.src="/Imagens/Black Cat End Game.gif"
-     cat.style.width = '75px'
-     cat.style.marginLeft = '50px'
-
-     clearInterval(loop);
-    }
-
+  count++;
+  score.innerHTML = `SCORE: ${count}`;
 }, 10);
-
-document.addEventListener('keydown', jump);    
